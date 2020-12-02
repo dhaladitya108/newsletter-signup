36df2d75dc8fd681cba36d149184453a5f12d942
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-12-02 20:55:46 +0530

    🚀 project completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -16,13 +16,45 @@ app.post("/", (req, res) => {
   let lastName = req.body.lname;
   let email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  let jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/fd4868450e";
+
+  const options = {
+    method: "POST",
+    auth: "adi:645bec17b2a67fba43c663866124ed1d-us7",
+  };
+
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode == 200) res.sendFile(__dirname + "/success.html");
+    else res.sendFile(__dirname + "/failure.html");
+
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, () => {
+app.use("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running at port 3000");
 });
-
-
-// apiKey
-// 
